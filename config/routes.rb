e1591b4a6e7120f2d0a1155961678c3654fb7b3b
Andrey <railscode@gmail.com>
2013-04-07 18:07:07 +0700

    фикс роутов

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # coding: utf-8
 Robokassa::Engine.routes.draw do
-  get "/:notification_key/notify" => :notify, :as => :robokassa_notification
-  get "/success" => :success, :as => :robokassa_on_success
-  get "/fail" => :fail, :as => :robokassa_on_fail
+  controller :robokassa do
+    get "/:notification_key/notify" => :notify, :as => :robokassa_notification
+    get "/success" => :success, :as => :robokassa_on_success
+    get "/fail" => :fail, :as => :robokassa_on_fail
+  end
 end
